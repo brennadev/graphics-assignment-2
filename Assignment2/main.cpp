c63d1f1d14cb67b8c7f1bcc167c7e55211a9d7d6
Brenna Olson <36449761+brennadev@users.noreply.github.com>
2018-10-04 08:53:16 -0500

    very beginning

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -7,9 +7,38 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+using namespace std;
+
+
+
+const int DEFAULT_WIDTH = 640;
+const int DEFAULT_HEIGHT = 480;
+
+const string DEFAULT_OUTPUT_FILE = "raytraced.bmp";
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    // make sure we have the correct number of arguments before anything is done
+    if (argc != 2) {
+        cout << "Incorrect number of arguments passed" << endl;
+        exit(1);
+    }
+    
+    ifstream sceneInputFile(argv[1]);
+
+    if (sceneInputFile.fail()) {
+        cout << "Unable to open input file" << endl;
+        exit(2);
+    }
+    
+    
+    
+    
+    
     return 0;
 }
